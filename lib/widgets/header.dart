52a6179a4715c9e6005701cedc4252ec2ed45ba8
Sakshi Ghadigaonkar <sakshig2507@gmail.com>
2021-10-12 18:13:27 +0530

    First Commit

---
+++ lib/widgets/header.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+AppBar header(context,
+    {bool isAppTitle = false, String titleText, removeBackButton = false}) {
+  return AppBar(
+    automaticallyImplyLeading: removeBackButton ? false : true,
+    title: Text(
+      isAppTitle ? "FlutterShare" : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? "Signatra" : "",
+        fontSize: isAppTitle ? 50.0 : 22.0,
+      ),
+      overflow: TextOverflow.ellipsis,
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
+}
